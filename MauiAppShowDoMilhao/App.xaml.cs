55997fef246a9a78c7c861d40af874cf1c8ae453
Usuario <Usuario@CPD03-M03>
2024-08-12 15:07:19 -0300

    Avanço

--- MauiAppShowDoMilhao/App.xaml.cs
+++ MauiAppShowDoMilhao/App.xaml.cs
@@ -397,16 +397,344 @@ namespace MauiAppShowDoMilhao
             new Pergunta
             {
                 Id = 13,
-                Enunciado = "Qual é o médico que mais utiliza o eletrocardiograma?",
+                Enunciado = "O FBI é a polícia federal de qual país?",
                 Alternativas = new ()
                 {
-                    new Alternativa { Correta = false, Descricao = "ORTOPEDISTA"},
-                    new Alternativa { Correta = false, Descricao = "NEUROLOGISTA"},
-                    new Alternativa { Correta = false, Descricao = "DERMATOLOGISTA" },
-                    new Alternativa { Correta = true, Descricao = "CARDIOLOGISTA" },
+                    new Alternativa { Correta = false, Descricao = "INGLATERRA"},
+                    new Alternativa { Correta = true, Descricao = "ESTADOS UNIDOS"},
+                    new Alternativa { Correta = false, Descricao = "CANADÁ" },
+                    new Alternativa { Correta = false, Descricao = "FRANÇA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 14,
+                Enunciado = "O Coliseu é um monumento histórico de que cidade européia?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "PARIS"},
+                    new Alternativa { Correta = false, Descricao = "COPENHAGUE"},
+                    new Alternativa { Correta = true, Descricao = "ROMA" },
+                    new Alternativa { Correta = false, Descricao = "LONDRES" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 15,
+                Enunciado = "Qual é o ponto mais alto da Terra?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = true, Descricao = "MONTE EVEREST"},
+                    new Alternativa { Correta = false, Descricao = "MONTE SINAI"},
+                    new Alternativa { Correta = false, Descricao = "MONTE CASTELO" },
+                    new Alternativa { Correta = false, Descricao = "MONT BLANC" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 16,
+                Enunciado = "Qual é a raça do personagem principal do filme “Beethoven”?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "FILA"},
+                    new Alternativa { Correta = false, Descricao = "PASTOR ALEMÃO"},
+                    new Alternativa { Correta = false, Descricao = "POODLE" },
+                    new Alternativa { Correta = true, Descricao = "SÃO BERNARDO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 17,
+                Enunciado = "Qual é a sigla da Organização das Nações Unidas?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = true, Descricao = "ONU"},
+                    new Alternativa { Correta = false, Descricao = "FMI"},
+                    new Alternativa { Correta = false, Descricao = "CIA" },
+                    new Alternativa { Correta = false, Descricao = "OTAN" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 18,
+                Enunciado = "Como é chamada a doença que causa perda desigual da melanina?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ASTIGMATISMO"},
+                    new Alternativa { Correta = false, Descricao = "TENDINITE"},
+                    new Alternativa { Correta = true, Descricao = "VITILIGO" },
+                    new Alternativa { Correta = false, Descricao = "DALTONISMO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 19,
+                Enunciado = "No filme, quem era parceiro de crimes da Bonnie?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "BILL"},
+                    new Alternativa { Correta = true, Descricao = "CLYDE"},
+                    new Alternativa { Correta = false, Descricao = "JAMES" },
+                    new Alternativa { Correta = false, Descricao = "BUTCH" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 20,
+                Enunciado = "Qual dessas cobras não é venenosa?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "URUTU"},
+                    new Alternativa { Correta = false, Descricao = "CASCAVEL"},
+                    new Alternativa { Correta = false, Descricao = "JARARACA" },
+                    new Alternativa { Correta = true, Descricao = "PÍTON" },
                 }
             },
         };
+        List<Pergunta> perguntas_dificeis = new()
+        {
+            new Pergunta
+            {
+                Id = 1,
+                Enunciado = "Onde nasceu Van Gogh, o grande pintor impressionista?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "POLÔNIA"},
+                    new Alternativa { Correta = false, Descricao = "FRANÇA"},
+                    new Alternativa { Correta = false, Descricao = "ITÁLIA" },
+                    new Alternativa { Correta = true, Descricao = "HOLANDA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 2,
+                Enunciado = "Qual desses países não fica na Ásia?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "PAQUISTÃO"},
+                    new Alternativa { Correta = false, Descricao = "JAPÃO"},
+                    new Alternativa { Correta = false, Descricao = "TAILÂNDIA" },
+                    new Alternativa { Correta = true, Descricao = "EGITO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 3,
+                Enunciado = "Qual é a primeira letra do alfabeto grego?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "DELTA"},
+                    new Alternativa { Correta = false, Descricao = "BETA"},
+                    new Alternativa { Correta = true, Descricao = "ALFA" },
+                    new Alternativa { Correta = false, Descricao = "GAMA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 4,
+                Enunciado = "Os nazistas foram julgados em:",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "BERLIM"},
+                    new Alternativa { Correta = true, Descricao = "NUREMBERGUE"},
+                    new Alternativa { Correta = false, Descricao = "MUNIQUE" },
+                    new Alternativa { Correta = false, Descricao = "PARIS" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 5,
+                Enunciado = "Que ramo da pecuária ocupase de bodes e cabras?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ASININO"},
+                    new Alternativa { Correta = true, Descricao = "CAPRINO"},
+                    new Alternativa { Correta = false, Descricao = "BUFALINO" },
+                    new Alternativa { Correta = false, Descricao = "MUAR" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 6,
+                Enunciado = "Que conflito ideológico envolveu os EUA e a União Soviética?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = true, Descricao = "GUERRA FRIA"},
+                    new Alternativa { Correta = false, Descricao = "GUERRA DO VIETNÃ"},
+                    new Alternativa { Correta = false, Descricao = "GUERRA NAS ESTRELAS" },
+                    new Alternativa { Correta = false, Descricao = "GUERRA DA CORÉIA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 7,
+                Enunciado = "Qual produto gerou guerras e conflitos no século XX?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ÁLCOOL"},
+                    new Alternativa { Correta = true, Descricao = "PETRÓLEO"},
+                    new Alternativa { Correta = false, Descricao = "OURO" },
+                    new Alternativa { Correta = false, Descricao = "ALUMÍNIO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 8,
+                Enunciado = "Como é chamada a bola de gelo e poeira que orbita em torno do sol?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "METEORITO"},
+                    new Alternativa { Correta = false, Descricao = "ESTRELA D’ALVA"},
+                    new Alternativa { Correta = false, Descricao = "CAMADA DE OZÔNIO" },
+                    new Alternativa { Correta = true, Descricao = "COMETA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 9,
+                Enunciado = "Que figura mitológica é conhecida por sua força extraordinária?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ÁTILA"},
+                    new Alternativa { Correta = false, Descricao = "MINOTAURO"},
+                    new Alternativa { Correta = false, Descricao = "PERSEU" },
+                    new Alternativa { Correta = true, Descricao = "HÉRCULES" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 10,
+                Enunciado = "Que símbolo está desenhado no centro da bandeira Argentina?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = true, Descricao = "SOL"},
+                    new Alternativa { Correta = false, Descricao = "LUA"},
+                    new Alternativa { Correta = false, Descricao = "ESTRELA" },
+                    new Alternativa { Correta = false, Descricao = "FOICE E MARTELO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 11,
+                Enunciado = "Quem construiu o primeiro telescópio astronômico completo?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "CYRUS MCCORMICK"},
+                    new Alternativa { Correta = false, Descricao = "THOMAS EDISON"},
+                    new Alternativa { Correta = false, Descricao = "MICHELANGELO" },
+                    new Alternativa { Correta = true, Descricao = "GALILEU GALILEI" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 12,
+                Enunciado = "Em que ano foi inaugurada a estátua do Cristo Redentor no Rio de Janeiro?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "1921"},
+                    new Alternativa { Correta = true, Descricao = "1931"},
+                    new Alternativa { Correta = false, Descricao = "1941" },
+                    new Alternativa { Correta = false, Descricao = "1951" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 13,
+                Enunciado = "Qual é o exame que emite ondas sonoras para visualizar os órgãos?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "XEROGRAFIA"},
+                    new Alternativa { Correta = false, Descricao = "RADIOGRAFIA"},
+                    new Alternativa { Correta = true, Descricao = "ULTRA-SONOGRAFIA" },
+                    new Alternativa { Correta = false, Descricao = "FOTOGRAFIA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 14,
+                Enunciado = "Quem escreveu “Dom Quixote”?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ESPINOZA"},
+                    new Alternativa { Correta = true, Descricao = "MIGUEL DE CERVANTES"},
+                    new Alternativa { Correta = false, Descricao = "CARLOS CONTE" },
+                    new Alternativa { Correta = false, Descricao = "ANGEL MORITA" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 15,
+                Enunciado = "Qual é o quarto planeta do sistema solar?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "JÚPITER"},
+                    new Alternativa { Correta = false, Descricao = "VÊNUS"},
+                    new Alternativa { Correta = false, Descricao = "TERRA" },
+                    new Alternativa { Correta = true, Descricao = "MARTE" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 16,
+                Enunciado = "A que país pertence a Groenlândia?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = true, Descricao = "DINAMARCA"},
+                    new Alternativa { Correta = false, Descricao = "INGLATERRA"},
+                    new Alternativa { Correta = false, Descricao = "NORUEGA" },
+                    new Alternativa { Correta = false, Descricao = "ESTADOS UNIDOS" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 17,
+                Enunciado = "Pablo Picasso foi um dos criadores do:",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "SURREALISMO"},
+                    new Alternativa { Correta = false, Descricao = "REALISMO"},
+                    new Alternativa { Correta = true, Descricao = "CUBISMO" },
+                    new Alternativa { Correta = false, Descricao = "IMPRESSIONISMO" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 18,
+                Enunciado = "Qual foi o primeiro presidente do Brasil eleito pelo povo?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "GETÚLIO VARGAS"},
+                    new Alternativa { Correta = true, Descricao = "PRUDENTE DE MORAES"},
+                    new Alternativa { Correta = false, Descricao = "WASHINGTON LUÍS" },
+                    new Alternativa { Correta = false, Descricao = "TANCREDO NEVES" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 19,
+                Enunciado = "Como o rei francês Luis XVI e sua esposa Maria Antonieta morreram?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "ENFORCADOS"},
+                    new Alternativa { Correta = false, Descricao = "FUZILADOS"},
+                    new Alternativa { Correta = true, Descricao = "GUILHOTINADOS" },
+                    new Alternativa { Correta = false, Descricao = "QUEIMADOS" },
+                }
+            },
+            new Pergunta
+            {
+                Id = 20,
+                Enunciado = "Como é chamada a gravura impressa sobre pranchas de madeira?",
+                Alternativas = new ()
+                {
+                    new Alternativa { Correta = false, Descricao = "MATEROGRAFIA"},
+                    new Alternativa { Correta = false, Descricao = "LITOGRAFIA"},
+                    new Alternativa { Correta = true, Descricao = "XILOGRAFIA" },
+                    new Alternativa { Correta = false, Descricao = "SERIGRAFIA" },
+                }
+            },
+        };
+
         public App()
         {
             InitializeComponent();
