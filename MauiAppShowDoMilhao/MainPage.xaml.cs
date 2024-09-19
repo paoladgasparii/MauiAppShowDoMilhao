b64fcd2ef1adbb9a9917d23cf8c6e76c62fef2b5
Usuario <Usuario@CPD03-M14>
2024-09-19 14:37:36 -0300

    Avanço

--- MauiAppShowDoMilhao/MainPage.xaml.cs
+++ MauiAppShowDoMilhao/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MauiAppShowDoMilhao.Models;
+using Plugin.Maui.Audio;
 
 namespace MauiAppShowDoMilhao
 {
@@ -18,7 +19,84 @@ namespace MauiAppShowDoMilhao
             lbl_premio.Text = premio.ToString("C");
             lbl_pergunta_numero.Text = pergunta_count.ToString();
 
+            // Adiciona som
+            Stream track =
+                FileSystem.OpenAppPackageFileAsync("abertura-show-do-milhao.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play();
+
         }
+        private void toca_som()
+        {
+            string track = "";
+
+            switch(pergunta_count)
+                {
+                case 1:
+                    track = "1000.wav";
+                    break;
+
+                case 2:
+                    track = "2000.wav";
+                    break;
+
+                case 3:
+                    track = "3000.wav";
+                    break;
+
+                case 4:
+                    track = "4000.wav";
+                    break;
+
+                case 5:
+                    track = "5000.wav";
+                    break;
+
+                case 6:
+                    track = "10000.wav";
+                    break;
+
+                case 7:
+                    track = "20000.wav";
+                    break;
+
+                case 8:
+                    track = "30000.wav";
+                    break;
+
+                case 9:
+                    track = "40000.wav";
+                    break;
+
+                case 10:
+                    track = "50000.wav";
+                    break;
+                case 11:
+                    track = "100000.wav";
+                    break;
+
+                case 12:
+                    track = "200000.wav";
+                    break;
+
+                case 13:
+                    track = "300000.wav";
+                    break;
+
+                case 14:
+                    track = "400000.wav";
+                    break;
+
+                case 15:
+                    track = "500000.wav";
+                    break;
+
+                case 16:
+                    track = "1000000.wav";
+                    break;
+
+            }
+            AudioManager.Current.CreatePlayer(
+                FileSystem.OpenAppPackageFileAsync(track).Result).Play();
 
         private async void Button_Clicked_Proxima(object sender, EventArgs e)
         {
@@ -72,6 +150,9 @@ namespace MauiAppShowDoMilhao
             else
             {
                 await DisplayAlert("ERROU!", "Você perdeu", "OK");
+                premio = 0;
+                pergunta_count = 1;
+                avanca_pergunta();
                 
             }
         }
